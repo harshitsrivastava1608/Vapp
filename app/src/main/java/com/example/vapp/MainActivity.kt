630675e3ae216f7379b3d53e2b7a489e90155147
harshitsrivastava1608 <harshit.srivastava1608@gmail.com>
2021-11-28 21:00:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.vapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+    }
+
+    fun add(view: android.view.View) {}
+}
